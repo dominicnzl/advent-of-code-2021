aefc0893c30a68c54f949af22030637962b7ae8d
dng <dominicnzl@hotmail.com>
2021-12-01 11:05:37 +0100

    heb je maven voor mij?

---
+++ src/test/java/ng/dominic/adventofcode/AdventOfCodeApplicationTests.java
@@ -0,0 +1,14 @@
+package ng.dominic.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("het begint?");
+    }
+
+}
