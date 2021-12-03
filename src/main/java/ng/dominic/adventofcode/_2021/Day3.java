11e537bf20020670865d8afe0016b1d1e5c307ca
dng <dominicnzl@hotmail.com>
2021-12-03 19:27:41 +0100

    rearranged the resources so I have to type less

--- src/main/java/ng/dominic/adventofcode/_2021/Day3.java
+++ src/main/java/ng/dominic/adventofcode/_2021/Day3.java
@@ -1,15 +1,90 @@
 package ng.dominic.adventofcode._2021;
 
+import java.io.IOException;
+import java.util.List;
+
+import static ng.dominic.adventofcode.util.Reader.read;
+
 public class Day3 {
 
     private Day3() {
     }
 
-    public static int task1() {
-        return 0;
+    public static int task1(String fileName, int positions) throws IOException {
+        try (var reader = read(fileName)) {
+            var count = new int[positions];
+            var lineCount = 0;
+            for (String line; (line = reader.readLine()) != null; ) {
+                if (line.length() != positions) {
+                    throw new IllegalArgumentException("denk dat het aantal positions niet klopt");
+                }
+                var chars = line.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    count[i] = count[i] + Character.getNumericValue(chars[i]);
+                }
+                lineCount++;
+            }
+            StringBuilder g = new StringBuilder();
+            StringBuilder e = new StringBuilder();
+            for (int i = 0; i < count.length; i++) {
+                // ik zie niet wat ik zou moeten doen bij een gelijk aantal 0 en 1 dus apart tellen
+                g.append(count[i] > lineCount / 2 ? "1" : "0");
+                e.append(count[i] < lineCount / 2 ? "1" : "0");
+            }
+            int gamma = Integer.parseInt(g.toString(), 2);
+            int epsilon = Integer.parseInt(e.toString(), 2);
+            return gamma * epsilon;
+        }
     }
 
-    public static int task2() {
-        return 0;
+    // ik geef het op, dan maar lelijk!
+    public static int task2(String fileName) throws IOException {
+        try (var reader = read(fileName)) {
+            var lines = reader.lines().toList();
+            final var numberChar = lines.get(0).length();
+
+            var c02lines = lines;
+
+            var o2String = "";
+            var oCol = 0;
+            for (int i = 0; i < numberChar; i++) {
+                var oneOrZero = getBitCriterium(lines, oCol, true);
+                int finalCol = oCol;
+                lines = lines.stream()
+                        .filter(s -> oneOrZero.equals(s.charAt(finalCol)))
+                        .toList();
+                if (lines.size() == 1) {
+                    o2String = lines.get(0);
+                    break;
+                }
+                oCol++;
+            }
+            var cO2String = "";
+            var cCol = 0;
+            for (int j = 0; j < numberChar; j++) {
+                var oneOrZero = getBitCriterium(c02lines, cCol, false);
+                int finalCCol = cCol;
+                c02lines = c02lines.stream()
+                        .filter(s -> oneOrZero.equals(s.charAt(finalCCol)))
+                        .toList();
+                if (c02lines.size() == 1) {
+                    cO2String = c02lines.get(0);
+                    break;
+                }
+                cCol++;
+            }
+            return Integer.parseInt(o2String, 2) * Integer.parseInt(cO2String, 2);
+        }
+    }
+
+    private static Character getBitCriterium(List<String> lines, int kolomindex, boolean isOxygen) {
+        var count = lines.stream()
+                .map(line -> line.charAt(kolomindex))
+                .mapToInt(Character::getNumericValue)
+                .sum();
+        if (isOxygen) {
+            return count >= (lines.size() + 1) / 2 ? '1' : '0';
+        }
+        return count < (lines.size() + 1) / 2 ? '1' : '0';
     }
 }
--- src/test/java/ng/dominic/adventofcode/_2021/Day3Test.java
+++ src/test/java/ng/dominic/adventofcode/_2021/Day3Test.java
@@ -1,7 +1,28 @@
 package ng.dominic.adventofcode._2021;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Day3Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Day3Test.class);
+
+    @Test
+    void task1() throws IOException {
+        // Integer.parseInt("10110", 2) * Integer.parseInt("01001", 2);
+        assertThat(Day3.task1("_2021/day3-sample", 5)).isEqualTo(198);
+        logger.info("Answer to task 1 is {}", Day3.task1("_2021/day3", 12));
+    }
+
+    @Test
+    void task2() throws IOException {
+        assertThat(Day3.task2("_2021/day3-sample")).isEqualTo(230);
+        logger.info("Answer to task2 is {}", Day3.task2("_2021/day3"));
+    }
 
 }
